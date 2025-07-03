64502cde5b13e00c15329578ad6742c65467fe19
Leonel <leonel.bruzzese@gmail.com>
2025-07-03 11:52:27 -0300

    Probando sessions.

--- src/app/components/photos-sessions/photos-sessions.component.ts
+++ src/app/components/photos-sessions/photos-sessions.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Gallery, GalleryItem, ImageItem } from 'ng-gallery';
 
 @Component({
   selector: 'app-photos-sessions',
@@ -7,5 +8,28 @@ import { Component } from '@angular/core';
   styleUrl: './photos-sessions.component.css'
 })
 export class PhotosSessionsComponent {
+
+  galleryId = 'albumesGallery';
+
+  constructor(private gallery: Gallery) {}
+
+  items: GalleryItem[] = [
+    // Albumes
+    new ImageItem({ src: '/assets/images/gallery/sessions/sessions-1.jpg', thumb: '/assets/images/gallery/sessions/sessions-1.jpg' }),
+    new ImageItem({ src: '/assets/images/gallery/sessions/sessions-2.jpg', thumb: '/assets/images/gallery/sessions/sessions-2.jpg' }),
+    new ImageItem({ src: '/assets/images/gallery/sessions/sessions-3.jpg', thumb: '/assets/images/gallery/sessions/sessions-3.jpg' }),
+    new ImageItem({ src: '/assets/images/gallery/sessions/sessions-4.jpg', thumb: '/assets/images/gallery/sessions/sessions-4.jpg' }),
+    new ImageItem({ src: '/assets/images/gallery/sessions/sessions-5.jpg', thumb: '/assets/images/gallery/sessions/sessions-5.jpg' }),
+    new ImageItem({ src: '/assets/images/gallery/sessions/sessions-6.jpg', thumb: '/assets/images/gallery/sessions/sessions-6.jpg' }),
+    new ImageItem({ src: '/assets/images/gallery/sessions/sessions-7.jpg', thumb: '/assets/images/gallery/sessions/sessions-7.jpg' }),
+    new ImageItem({ src: '/assets/images/gallery/sessions/sessions-8.jpg', thumb: '/assets/images/gallery/sessions/sessions-8.jpg' }),
+    new ImageItem({ src: '/assets/images/gallery/sessions/sessions-9.jpg', thumb: '/assets/images/gallery/sessions/sessions-9.jpg' }),
+  ];
+
+  ngOnInit(): void {
+    // Cargar los elementos de la galería
+    const galleryRef = this.gallery.ref(this.galleryId); // Le paso una referencia para que no me abra imagenes de otra galería.
+    galleryRef.load(this.items);
+  }
 
 }
