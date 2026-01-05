fcd802249a32fa22ac3745c85890d5a0defe2315
Leonel <leonel.bruzzese@gmail.com>
2026-01-05 12:26:11 -0300

    Fix galeria fanart.

--- src/app/components/photos-fanarts/photos-fanarts.component.ts
+++ src/app/components/photos-fanarts/photos-fanarts.component.ts
@@ -1,34 +1,25 @@
 import { Component } from '@angular/core';
-import { Gallery, GalleryItem, ImageItem } from 'ng-gallery';
 
 @Component({
   selector: 'app-photos-fanarts',
   standalone: false,
   templateUrl: './photos-fanarts.component.html',
-  styleUrl: './photos-fanarts.component.css'
+  styleUrls: ['./photos-fanarts.component.css']
 })
 export class PhotosFanartsComponent {
 
-  galleryId = 'albumesGallery';
+  // Para controlar cuál imagen está abierta
+  openedImage: { src: string, text: string } | null = null;
 
-  constructor(private gallery: Gallery) {}
+  // Método para abrir popup
+  openImage(src: string, text: string) {
+    this.openedImage = { src, text };
+  }
 
-  items: GalleryItem[] = [
-    // Albumes
-    new ImageItem({ src: '/assets/images/gallery/fanarts/fanart-1.jpg', thumb: '/assets/images/gallery/fanarts/fanart-1.jpg' }),
-    new ImageItem({ src: '/assets/images/gallery/fanarts/fanart-2.jpg', thumb: '/assets/images/gallery/fanarts/fanart-2.jpg' }),
-    new ImageItem({ src: '/assets/images/gallery/fanarts/fanart-3.jpg', thumb: '/assets/images/gallery/fanarts/fanart-3.jpg' }),
-    new ImageItem({ src: '/assets/images/gallery/fanarts/fanart-4.jpg', thumb: '/assets/images/gallery/fanarts/fanart-4.jpg' }),
-    new ImageItem({ src: '/assets/images/gallery/fanarts/fanart-5.jpg', thumb: '/assets/images/gallery/fanarts/fanart-5.jpg' }),
-    new ImageItem({ src: '/assets/images/gallery/fanarts/fanart-6.jpg', thumb: '/assets/images/gallery/fanarts/fanart-6.jpg' }),
-    new ImageItem({ src: '/assets/images/gallery/fanarts/fanart-7.jpg', thumb: '/assets/images/gallery/fanarts/fanart-7.jpg' }),
-    new ImageItem({ src: '/assets/images/gallery/fanarts/fanart-8.jpg', thumb: '/assets/images/gallery/fanarts/fanart-8.jpg' })
-  ];
-
-  ngOnInit(): void {
-    // Cargar los elementos de la galería
-    const galleryRef = this.gallery.ref(this.galleryId); // Le paso una referencia para que no me abra imagenes de otra galería.
-    galleryRef.load(this.items);
+  // Cerrar popup
+  closeImage() {
+    this.openedImage = null;
   }
 
 }
+
